2ada41db407181c05e12b693359d01250c505113
Matthias Geier <Matthias.Geier@gmail.com>
2020-04-22 17:25:38 +0200

    Add tests

---
+++ tests/test_example_repo.py
@@ -0,0 +1,85 @@
+import os
+from pathlib import Path
+import tempfile
+import time
+
+import pytest
+from sphinx.cmd.build import build_main
+
+
+# Set timezone to make tests reproducible
+os.environ['TZ'] = 'Europe/Berlin'
+time.tzset()
+
+time1 = '2020-04-22 12:41:20 GMT+02:00'
+time2 = '2020-04-23 09:24:08 GMT+02:00'
+
+expected_results = {
+    'index': [time1, 'defined'],
+    'api': [time2, 'defined'],
+    'example_module.example_function': [time2, 'undefined'],
+    'search': ['None', 'undefined'],
+}
+
+test_dir = Path(__file__).parent
+
+
+def run_sphinx(srcdir, **kwargs):
+    with tempfile.TemporaryDirectory() as outdir:
+        args = [str(srcdir), outdir, '-W']
+        args.extend('-D{}={}'.format(k, v) for k, v in kwargs.items())
+        result = build_main(args)
+        assert result == 0
+        outdir = Path(outdir)
+        data = {}
+        for name in expected_results:
+            path = outdir / (name + '.html')
+            data[name] = path.read_text().splitlines()
+    return data
+
+
+def test_repo_full():
+    data = run_sphinx(test_dir / 'repo_full')
+    assert data == expected_results
+
+
+def test_untracked_no_dependencies():
+    data = run_sphinx(
+        test_dir / 'repo_full',
+        git_untracked_check_dependencies=0,
+    )
+    assert data == {
+        **expected_results,
+        'example_module.example_function': ['None', 'undefined'],
+    }
+
+
+def test_untracked_show_sourcelink():
+    data = run_sphinx(
+        test_dir / 'repo_full',
+        git_untracked_show_sourcelink=1,
+    )
+    assert data == {
+        **expected_results,
+        'example_module.example_function': [time2, 'defined'],
+    }
+
+
+def test_untracked_no_dependencies_and_show_sourcelink():
+    data = run_sphinx(
+        test_dir / 'repo_full',
+        git_untracked_check_dependencies=0,
+        git_untracked_show_sourcelink=1,
+    )
+    assert data == {
+        **expected_results,
+        'example_module.example_function': ['None', 'defined'],
+    }
+
+
+def test_repo_shallow():
+    data = run_sphinx(test_dir / 'repo_shallow')
+    assert data == {
+        **expected_results,
+        'index': ['None', 'defined'],
+    }
---
+++ tests/update_submodules.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import subprocess
+
+
+path = Path(__file__).parent
+
+
+def update_submodule(name, depth):
+    subprocess.run(
+        ['git', 'submodule', 'update', '--init', '--depth', str(depth), name],
+        cwd=path,
+        check=True,
+    )
+
+
+if __name__ == '__main__':
+    update_submodule('repo_full', 0x7fffffff)
+    update_submodule('repo_shallow', 3)
